a0e8c4b54deb452e9df144558a42b7778b1966c1
Brandon2002 <btapia@ucol.mx>
2020-12-04 18:18:59 -0600

    implemented contarImparesWhile()

--- src/main.js
+++ src/main.js
@@ -30,14 +30,16 @@ export default class App {
         return suma;
     }
     contarImparesWhile(inicio, fin){
+
         let i = inicio;
         let impares = 0;
+
         while(i <= fin){
 
-            if(i%2 !== 0){
-                impares++;
+            if(i % 2 != 0){
+                impares = impares + 1;
             }
-            i++;
+            i= i + 1;
         }
         return impares;
     }
@@ -76,7 +78,7 @@ let app = new App();
 console.log(app.sumarParesFor());
 console.log(app.contarImparesFor(5, 10));
 console.log(app.sumarParesWhile());
-console.log(app.contarImparesWhile(5, 11));
+console.log(app.contarImparesWhile(5, 10));
 console.log(app.sumarParesDo());
 console.log(app.contarImparesDo(5, 11));
 
